6912c2362904a36572a47a2733d75fafb8e01d62
mmmaaaxxx77 <mmmaaaxxx77@gmail.com>
2017-08-03 23:47:30 +0800

    no message

--- ml-train/senti/test_lstm.py
+++ ml-train/senti/test_lstm.py
@@ -174,8 +174,8 @@ test_y = to_categorical(test_y, nb_classes=2)
 print("{}, {}".format(np.array(x_train).shape, np.array(y_train).shape))
 model = tflearn.DNN(net, tensorboard_verbose=0, tensorboard_dir="log/")
 
-model.fit(x_train, y_train, validation_set=(test_x, test_y), show_metric=True,
-          batch_size=128, run_id="lstm_senti_2t2", n_epoch=30)
+model.fit(x_train, y_train, validation_set=0.2, show_metric=True,
+          batch_size=128, run_id="lstm_senti_2t2", n_epoch=3)
 
 
 def pre_clear(sentence):
