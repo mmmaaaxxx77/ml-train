2577fd2222700c998b00fd7c8d66c29d3395a54e
mmmaaaxxx77 <mmmaaaxxx77@gmail.com>
2017-08-10 19:01:27 +0800

    no message

--- ml-train/senti/testt.py
+++ ml-train/senti/testt.py
@@ -1,4 +1,8 @@
+import jieba
+import numpy as np
 import re
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.feature_extraction.text import HashingVectorizer
 from tflearn.data_utils import pad_sequences
 
 r2 = r'https://[a-zA-Z0-9.?/&=:]*'
@@ -7,3 +11,23 @@ line = re.sub(r2, '', "$$$  測  試https://zhidao.baidu.com/question/1830964875
 line = re.sub(r, '', line)
 print(line)
 
+comma_tokenizer = lambda x: jieba.cut(x, cut_all=False, HMM=True)
+
+vect = FeatureHasher(n_features=2**21, non_negative=True)
+
+terms = comma_tokenizer("柯P是柯市長")
+
+terms = [{t: 1} for t in terms]
+print(terms)
+print(vect.transform(terms)[0].nonzero()[1][0])
+#print(vect.transform(terms).toarray())
+
+
+def transform_term_to_id(sentence):
+    terms = comma_tokenizer(sentence)
+    terms = [{t: 1} for t in terms]
+    ids_sci = vect.transform(terms)
+    return np.asarray([item.nonzero()[1][0] for item in ids_sci])
+
+print(transform_term_to_id("柯P柯P是柯市長"))
+print(transform_term_to_id("柯P柯P是柯市長").size)
