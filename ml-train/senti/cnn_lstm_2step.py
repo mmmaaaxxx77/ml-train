2fced78f7df119c8ba4a556f1c709df080f32790
mmmaaaxxx77 <mmmaaaxxx77@gmail.com>
2017-08-24 18:47:10 +0800

    no message

--- ml-train/senti/cnn_lstm_2step.py
+++ ml-train/senti/cnn_lstm_2step.py
@@ -8,10 +8,49 @@ from sklearn.feature_extraction import FeatureHasher
 from tflearn import embedding, bidirectional_rnn, BasicLSTMCell
 from tflearn.data_utils import to_categorical
 import jieba.posseg as pseg
-from senti import logconf
 import tensorflow as tf
 
-logger = logconf.Logger(__file__).logger
+from colorlog import ColoredFormatter
+import logging
+
+
+class Logger:
+    """Basic logging configuration."""
+
+    def __init__(self, logger_name):
+        """init."""
+        formatter = ColoredFormatter(
+            '%(log_color)s%(levelname)-5s%(reset)s '
+            '%(yellow)s[%(asctime)s]%(reset)s%(white)s '
+            '%(name)s %(funcName)s %(bold_purple)s:%(lineno)d%(reset)s '
+            '%(log_color)s%(message)s%(reset)s',
+            datefmt='%y-%m-%d %H:%M:%S',
+            log_colors={
+                'DEBUG': 'blue',
+                'INFO': 'bold_cyan',
+                'WARNING': 'red,',
+                'ERROR': 'bg_bold_red',
+                'CRITICAL': 'red,bg_white',
+            }
+        )
+
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+
+        # StreamHandler
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.DEBUG)
+        sh.setFormatter(formatter)
+
+        # Add handlers
+        logger.addHandler(sh)
+
+        self.logger = logger
+
+    def __getattr__(self, name):  # noqa
+        return getattr(self.logger, name)
+
+logger = Logger(__file__).logger
 
 jieba.load_userdict("dict/dict.txt.big.txt")
 jieba.load_userdict("dict/NameDict_Ch_v2")
